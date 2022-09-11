a09b129f1d466729f634f871d80d25d609914116
Jorge Cano <jorgecano86@gmail.com>
2022-09-11 13:54:57 +0200

    Fix tests

--- test/NewCelebrities.UnitTests/GameCreatorShould.cs
+++ test/NewCelebrities.UnitTests/GameCreatorShould.cs
@@ -12,7 +12,7 @@ namespace NewCelebrities.UnitTests
             var lines = File.ReadAllLines(sourcePath);
             var characters = Core.File.Reader.Read(lines).ToList();
             var selectedCharacters = characters.RandomizeList().Take(40);
-            var game = new Game(3, 30, selectedCharacters, 
+            var game = new Game(3, 30, false, selectedCharacters, 
                 new[] { Color.DarkParty, Color.Lemon }.Select(x => new Team(x)).ToList());
 
             game.Deck.Count.Should().Be(40);
